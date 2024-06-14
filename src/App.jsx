875fba65409dd74ffd2b2699a3d85110eb749987
Mayank Sethi <mayank@digibrain.co.in>
2024-06-15 03:12:29 +0530

    add result section to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import { calculateInvestmentResults, formatter } from './util/investment';
 
 const DEBUG = false;
 
@@ -11,8 +12,32 @@ const defaultUserInput = {
   duration: 12,
 };
 
-const Results = () => {
-  return <p>Results...</p>;
+const Results = ({ input }) => {
+  const result = calculateInvestmentResults(input);
+  return (
+    <>
+      <table id="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Interest</th>
+            <th>Value End Of Year</th>
+            <th>Annual Investment</th>
+          </tr>
+        </thead>
+        <tbody>
+          {result.map((row, index) => (
+            <tr key={index}>
+              <td>{row.year}</td>
+              <td>{formatter.format(row.interest)}</td>
+              <td>{formatter.format(row.valueEndOfYear)}</td>
+              <td>{formatter.format(row.annualInvestment)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 const App = () => {
@@ -43,7 +68,7 @@ const App = () => {
     <>
       <Header />
       <UserInput onChange={handleChange} userInput={userInput} />
-      <Results />
+      <Results input={userInput} />
     </>
   );
 };
