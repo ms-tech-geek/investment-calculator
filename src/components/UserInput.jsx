0b41469a024031e5b3d9ad9398b2a710b28f9376
Mayank Sethi <mayank@digibrain.co.in>
2024-06-14 20:26:01 +0530

    add userinput layout and handle onChange event

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react';
+
+const DEBUG = false;
+
+const defaultUserInput = {
+  initialInvestment: 15000,
+  annualInvestment: 120000,
+  expectedReturn: 7,
+  duration: 12,
+};
+
+const UserInput = () => {
+  const [userInput, setUserInput] = useState(defaultUserInput);
+
+  const handleChange = ({ inputIdentifier, newValue }) => {
+    DEBUG &&
+      console.log(
+        `handleChange.args`,
+        JSON.stringify({ inputIdentifier, newValue })
+      );
+
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+
+    DEBUG &&
+      console.log(`handleChange.setUserInput`, JSON.stringify(userInput));
+  };
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange({
+                inputIdentifier: 'initialInvestment',
+                newValue: event.target.value,
+              })
+            }
+          />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange({
+                inputIdentifier: 'annualInvestment',
+                newValue: event.target.value,
+              })
+            }
+          />
+        </p>
+        <p>
+          <label>Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange({
+                inputIdentifier: 'expectedReturn',
+                newValue: event.target.value,
+              })
+            }
+          />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) =>
+              handleChange({
+                inputIdentifier: 'duration',
+                newValue: event.target.value,
+              })
+            }
+          />
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default UserInput;
