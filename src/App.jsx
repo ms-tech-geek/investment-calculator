b61951921f845bf2156632c169c44ad79663f703
Mayank Sethi <mayank@digibrain.co.in>
2024-06-14 20:55:04 +0530

    lift up userInput state to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
 
+const DEBUG = false;
+
+const defaultUserInput = {
+  initialInvestment: 15000,
+  annualInvestment: 120000,
+  expectedReturn: 7,
+  duration: 12,
+};
+
+const Results = () => {
+  return <p>Results...</p>;
+};
+
 const App = () => {
+  const [userInput, setUserInput] = useState(defaultUserInput);
+
+  DEBUG &&
+    console.log(`UserInput.defaultUserInput`, JSON.stringify(defaultUserInput));
+
+  const handleChange = ({ inputIdentifier, newValue }) => {
+    DEBUG &&
+      console.log(
+        `handleChange.args`,
+        JSON.stringify({ inputIdentifier, newValue })
+      );
+
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+
+    DEBUG &&
+      console.log(`handleChange.setUserInput`, JSON.stringify(userInput));
+  };
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      <Results />
     </>
   );
 };
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,38 +1,4 @@
-import { useState } from 'react';
-
-const DEBUG = false;
-
-const defaultUserInput = {
-  initialInvestment: 15000,
-  annualInvestment: 120000,
-  expectedReturn: 7,
-  duration: 12,
-};
-
-const UserInput = () => {
-  const [userInput, setUserInput] = useState(defaultUserInput);
-
-  DEBUG &&
-    console.log(`UserInput.defaultUserInput`, JSON.stringify(defaultUserInput));
-
-  const handleChange = ({ inputIdentifier, newValue }) => {
-    DEBUG &&
-      console.log(
-        `handleChange.args`,
-        JSON.stringify({ inputIdentifier, newValue })
-      );
-
-    setUserInput((prevUserInput) => {
-      return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
-      };
-    });
-
-    DEBUG &&
-      console.log(`handleChange.setUserInput`, JSON.stringify(userInput));
-  };
-
+const UserInput = ({ onChange, userInput }) => {
   return (
     <section id="user-input">
       <div className="input-group">
@@ -43,7 +9,7 @@ const UserInput = () => {
             required
             value={userInput.initialInvestment}
             onChange={(event) =>
-              handleChange({
+              onChange({
                 inputIdentifier: 'initialInvestment',
                 newValue: event.target.value,
               })
@@ -57,7 +23,7 @@ const UserInput = () => {
             required
             value={userInput.annualInvestment}
             onChange={(event) =>
-              handleChange({
+              onChange({
                 inputIdentifier: 'annualInvestment',
                 newValue: event.target.value,
               })
@@ -73,7 +39,7 @@ const UserInput = () => {
             required
             value={userInput.expectedReturn}
             onChange={(event) =>
-              handleChange({
+              onChange({
                 inputIdentifier: 'expectedReturn',
                 newValue: event.target.value,
               })
@@ -87,7 +53,7 @@ const UserInput = () => {
             required
             value={userInput.duration}
             onChange={(event) =>
-              handleChange({
+              onChange({
                 inputIdentifier: 'duration',
                 newValue: event.target.value,
               })
